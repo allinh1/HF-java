13754f012f1ca5b6aeebd93c9777ab7eb6afb550
allinh1 <dl.nguyen1322@gmail.com>
2021-08-31 14:32:59 +1000

    commit with gb

---
+++ src/com/company/DVDPlayer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class DVDPlayer
+{
+    boolean canRecord = false;
+
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+
+    void playDVD() {
+        System.out.println("DVD playing");
+    }
+}
