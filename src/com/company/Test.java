7c2bce872c0cc76735d3c01c6abcd42ca3289021
allinh1 <dl.nguyen1322@gmail.com>
2021-08-31 14:32:06 +1000

    second commit with IJ

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,28 +3,51 @@ package com.company;
 public class Test {
     public static void main(String[] args) {
 
-        Dog d = new Dog(); // make the Dog object 'd'
-        d.size = 40; // dot operator to set d.size to int 40.
-        d.bark(); // call bark method in d
-
-        System.out.println(d.size);
 
 
-        Movie one = new Movie();
-        one.title = "Gone with the Stock";
-        one.genre = "Tragic";
-        one.rating = -2;
 
-        Movie two = new Movie();
-        two.title = "Lost in Cubicle Space";
-        two.genre = "Comedy";
-        two.rating = 5;
-        two.playIt();
+//        TapeDeck t = new TapeDeck();
+////        t.canRecord = true;
+//        t.playTape();
+//
+//        if (t.canRecord == true) {
+//            t.recordTape();
+//        }
 
-        Movie three = new Movie();
-        one.title = "Space Jam";
-        one.genre = "Drama";
-        one.rating = 127;
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+
+        if (d.canRecord == true) {
+            d.recordDVD();
+        }
+
+
+//        Dog d = new Dog(); // make the Dog object 'd'
+//        d.size = 40; // dot operator to set d.size to int 40.
+//        d.bark(); // call bark method in d
+//
+//        System.out.println(d.size);
+
+//    This is not OO
+//        Movie one = new Movie();
+//        one.title = "Gone with the Stock";
+//        one.genre = "Tragic";
+//        one.rating = -2;
+//
+//        Movie two = new Movie();
+//        two.title = "Lost in Cubicle Space";
+//        two.genre = "Comedy";
+//        two.rating = 5;
+//        two.playIt();
+//        System.out.println(one.title);
+//
+//        Movie three = new Movie();
+//        three.title = "Space Jam";
+//        three.genre = "Drama";
+//        three.rating = 127;
+
+
     }
 }
 
