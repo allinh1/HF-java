b40ac597723da727c93ce8cf8de15f9d3e1b57af
allinh1 <dl.nguyen1322@gmail.com>
2021-09-01 13:40:55 +1000

    variables objects added

---
+++ src/com/company/Books.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Books {
+    String author;
+    String title;
+}
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,8 +1,74 @@
 package com.company;
 
 public class Test {
+
     public static void main(String[] args) {
 
+        Books[] myBooks = new Books[3];
+
+        int x = 0;
+
+        myBooks[0]= new Books();
+        myBooks[1] = new Books();
+        myBooks[2] = new Books();
+
+        myBooks[0].title = "Book1";
+        myBooks[1].title = "Book2";
+        myBooks[2].title = "Book3";
+        myBooks[0].author = "A1";
+        myBooks[1].author = "A2";
+        myBooks[2].author = "A3";
+
+        System.out.println(myBooks[0].title);
+
+        while (x < myBooks.length) {
+            System.out.print(myBooks[x].title);
+            System.out.print(" by ");
+            System.out.println(myBooks[x].author);
+            x = x + 1;
+        }
+
+
+//        Dog dog1 = new Dog();
+//        dog1.bark();
+//        dog1.name = "Bart";
+//
+//        // Dog Array
+//        Dog[] myDogs = new Dog[3];
+//
+//        myDogs[0] = new Dog();
+//        myDogs[1] = new Dog();
+//        myDogs[2] = dog1;
+//
+//        myDogs[0].name = "Fred";
+//        myDogs[1].name = "Marge";
+//
+//        System.out.println(myDogs[2].name);
+//        myDogs[2].bark();
+//        System.out.println(myDogs[0].name);
+//        System.out.println(myDogs[1].name);
+//
+//        int x = 0;
+//        while (x < myDogs.length) {
+//            myDogs[x].bark();
+//            x++;
+//        }
+
+
+
+
+
+
+//        DrumKit d = new DrumKit();
+//        d.playSnare();
+//        d.snare = false;
+//        d.playTopHat();
+//
+//
+//
+//        if (d.snare) {
+//            d.playSnare();
+//        }
 
 
 
@@ -14,13 +80,13 @@ public class Test {
 //            t.recordTape();
 //        }
 
-        DVDPlayer d = new DVDPlayer();
-        d.canRecord = true;
-        d.playDVD();
-
-        if (d.canRecord == true) {
-            d.recordDVD();
-        }
+//        DVDPlayer d = new DVDPlayer();
+//        d.canRecord = true;
+//        d.playDVD();
+//
+//        if (d.canRecord) {
+//            d.recordDVD();
+//        }
 
 
 //        Dog d = new Dog(); // make the Dog object 'd'
